387a101d3581deb3e412999b6e3f9163aa324169
NetFlex <84316160+Geruays@users.noreply.github.com>
2021-10-18 20:46:53 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+echo installing MultiSH
+
+echo pls wait
+
+
+
+curl https://raw.githubusercontent.com/Geruays/multi-sh/main/multish -o multish
+
+cp multish /bin
+
+cd /bin
+chmod +x multish
+
+cd $HOME
+
+echo Done Now Run With multish
+
+clear
